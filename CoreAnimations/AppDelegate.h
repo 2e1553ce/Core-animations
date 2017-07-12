5a8e48b9c1b6c8159fa68930d1e081173b0ce72e
aiuar <aivar_xid@mail.ru>
2017-07-12 22:09:59 +0300

    Initial Commit

---
+++ CoreAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimations
+//
+//  Created by aiuar on 12.07.17.
+//  Copyright © 2017 A.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimations
+//
+//  Created by aiuar on 12.07.17.
+//  Copyright © 2017 A.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
